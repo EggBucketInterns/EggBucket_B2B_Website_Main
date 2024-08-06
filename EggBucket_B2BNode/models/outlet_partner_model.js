dd7d15da2b40743f758c8daba98193d640ebfc9b
EggBucket <eggbucket6@gmail.com>
2024-08-06 10:35:21 +0530

    Add files via upload

---
+++ EggBucket_B2BNode/models/outlet_partner_model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const outletPartnerSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String },
+
+  phoneNumber: { type: String, required: true, unique: true },
+
+  aadharNumber: {
+    type: String,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  img: String,
+});
+
+const OutletPartner = mongoose.model("OutletPartner", outletPartnerSchema);
+
+module.exports = OutletPartner;
