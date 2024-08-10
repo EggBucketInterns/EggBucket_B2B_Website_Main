ddbfb5a445c43eca6a4748072ddad12b2ca83131
shashank_naik <shashanknaik39037@gmail.com>
2024-08-10 17:03:09 +0530

    Delete EggBucket_B2BNode directory

--- EggBucket_B2BNode/models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://localhost:27017/EggBucket_B2B")
-  .then(async () => {
-    console.log("Database connected!");
-  })
-  .catch((err) => console.error("Error connecting to database:", err));
