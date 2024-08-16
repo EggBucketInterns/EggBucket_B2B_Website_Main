e07fd969f8caca8ce092e04d6deabf453694a01a
shashank_naik <shashanknaik39037@gmail.com>
2024-08-17 02:26:40 +0530

    Delete EggBucket_backend directory

--- EggBucket_backend/routes/admin.js
+++
@@ -1,7 +0,0 @@
-const {orderAggregate}=require('../controllers/admin_controller')
-const express=require('express')
-const router=express.Router()
-
-router.get('/egg-bucket-b2b/dashboard',orderAggregate)
-
-module.exports=router
